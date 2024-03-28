e685a317fff1e76123c9fa7109a8ead6c62d3fd7
albertostizzoli <albertostizzoli@yahoo.it>
2024-03-28 14:52:36 +0100

    complete snack 1

--- src/org/experis/exercise/SnackOne.java
+++ src/org/experis/exercise/SnackOne.java
@@ -1,4 +1,22 @@
 package org.experis.exercise;
 
+import java.util.Scanner;
+
 public class SnackOne {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean numbers = false;
+
+        while(!numbers){
+            System.out.println("Inserisci un numero tra 10 e 20: ");
+            int number = scan.nextInt();
+            if(number >= 10 && number <= 20) {
+                numbers = true;
+                System.out.println("Giusto!!");
+            }else{
+                System.out.println("Numero non valido. Riprova");
+            }
+        }
+        scan.close();
+    }
 }
